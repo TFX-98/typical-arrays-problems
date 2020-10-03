5aa867ff1ff1d3fe11aa9e41c5e85d2e2c2249e6
TFX-98 <66826440+TFX-98@users.noreply.github.com>
2020-10-03 20:31:29 +0300

    Add Solution

--- src/index.js
+++ src/index.js
@@ -1,12 +1,42 @@
+exports.min = function min(array) {
 
-exports.min = function min (array) {
-  return 0;
+    let minNumber = 0;
+
+    if (array && array.length > 0) {
+        for (let i = 0; i < array.length; i++) {
+            if (minNumber > array[i]) {
+                minNumber = array[i];
+            }
+        }
+
+    }
+    return minNumber;
 }
 
-exports.max = function max (array) {
-  return 0;
+exports.max = function max(array) {
+
+    let maxNumber = 0;
+    if (array && array.length > 0) {
+        for (let i = 0; i < array.length; i++) {
+            if (maxNumber < array[i]) {
+                maxNumber = array[i];
+            }
+        }
+
+    }
+    return maxNumber;
 }
 
-exports.avg = function avg (array) {
-  return 0;
+exports.avg = function avg(array) {
+
+    let sum = 0;
+    let avg = 0;
+    if (array && array.length > 0) {
+        for (let i = 0; i < array.length; i++) {
+            sum += array[i];
+        }
+
+        avg = sum / array.length;
+    }
+    return avg;
 }
